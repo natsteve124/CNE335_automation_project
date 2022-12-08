27be1c4a4849df8e45b604057d215507e8ba9b20
natsteve124 <107065263+natsteve124@users.noreply.github.com>
2022-12-07 21:41:38 -0800

    SSH
    Implemented SSH Function

--- Server.py
+++ Server.py
@@ -3,13 +3,22 @@ import paramiko
 
 class Server:
     """ Server class for representing and manipulating servers. """
-    
     def __init__(self, server_ip):
-        # TODO -
         self.server_ip = server_ip
-        
 
     def ping(self):
         # TODO - Use os module to ping the server
         result = os.system("ping -n 5 %s" % self.server_ip)
         return result 
+
+    def ssh(self, ec2_username, path, command):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(self.server_ip, username = ec2_username, key_filename = path)
+
+        stdin, stdout, stderr = ssh.exec_command(command)
+        output = stdout.read().decode("utf-8").split("\n")
+        for message in output:
+            print(message)
+        
+        ssh.close()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Server import Server
+import os.path
 # This is the template code for the CNE335 Final Project
 # Nathaniel Stevens
 # CNE 335 Fall
@@ -10,6 +11,16 @@ def print_program_info():
 print_program_info()
 
 # TODO - Create a Server object
-S_object = Server("35.89.172.138")
+ec2_ip = "{INSERT IP HERE}"
+S_object = Server(ec2_ip)
 # TODO - Call Ping method and print the results
+print("pinging ec2 instance")
 S_object.ping()
+print("\nfinished pinging ec2 instance")
+
+P_filepath = os.path.join(os.path.expanduser('~'), ".ssh", "id_rsa")
+username = "ubuntu"
+command = "sudo apt update && sudo apt upgrade -y"
+print("\nssh to ec2 instance\n")
+S_object.ssh(username, P_filepath, command)
+print("finished ssh to ec2 instance")
