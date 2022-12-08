8d8ce08f0bea3fb4fadb5219b6ecbb849d9166e8
natsteve124 <107065263+natsteve124@users.noreply.github.com>
2022-12-07 20:43:48 -0800

    Created Ping
    Updated / Created the program to ping from python.

--- Server.py
+++ Server.py
@@ -1,10 +1,15 @@
+import os
+import paramiko
+
 class Server:
     """ Server class for representing and manipulating servers. """
-
+    
     def __init__(self, server_ip):
         # TODO -
         self.server_ip = server_ip
+        
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        result = os.system("ping -n 5 %s" % self.server_ip)
+        return result 
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+from Server import Server
 # This is the template code for the CNE335 Final Project
-# Justin Ellis
+# Nathaniel Stevens
 # CNE 335 Fall
 
 def print_program_info():
-    # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Nathaniel Stevens")
 
 # This is the entry point to our program
-if __name__ == '__main__':
-    print_program_info()
-    # TODO - Create a Server object
-    # TODO - Call Ping method and print the results
+print_program_info()
+
+# TODO - Create a Server object
+S_object = Server("35.89.172.138")
+# TODO - Call Ping method and print the results
+S_object.ping()
